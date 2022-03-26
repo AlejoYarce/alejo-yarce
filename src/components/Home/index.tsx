b5eca294dfd926643693f1f71a1dfb8b6050166b
AlejoYarce <alejo.yarce990@gmail.com>
2022-03-26 14:39:44 -0500

    home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import { HomeContainer, ContentContainer, InnerContentContainer } from './styles'
+import React, { useEffect, useState } from 'react'
 
+import {
+  HomeContainer,
+  ContentContainer,
+  InnerContentContainer,
+  WhiteContent,
+  ImageContent,
+  ImageContainer,
+} from './styles'
+import { RATIO } from '~app/utils/constants'
+
+let intervalId
 const Home: React.FC = () => {
+  const [fullWidthRatio, setFullWidthRatio] = useState(100)
+  const [showInnerContainer, setShowInnerContainer] = useState(false)
+  const [innerWidthRatio, setInnerWidthRatio] = useState(0)
+  const [showInnerContent, setShowInnerContent] = useState(false)
+
+  const intervalWidth = () => {
+    setShowInnerContainer(true)
+    let newRatio = 0
+    intervalId = setInterval(() => {
+      newRatio += RATIO
+
+      if (newRatio >= 80) {
+        newRatio = 80
+        clearInterval(intervalId)
+        setShowInnerContent(true)
+      }
+
+      setInnerWidthRatio(newRatio)
+    }, 10)
+  }
+
+  useEffect(() => {
+    let newRatio = 100
+    setTimeout(() => {
+      intervalId = setInterval(() => {
+        newRatio -= RATIO
+
+        if (newRatio <= 50) {
+          newRatio = 50
+          clearInterval(intervalId)
+          intervalWidth()
+        }
+
+        setFullWidthRatio(newRatio)
+      }, 10)
+    }, 500)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   return (
-    <HomeContainer>
-      <ContentContainer>
-        <InnerContentContainer />
-      </ContentContainer>
+    <HomeContainer widthRatio={fullWidthRatio}>
+      {showInnerContainer && (
+        <ContentContainer widthRatio={innerWidthRatio}>
+          <InnerContentContainer>
+            <WhiteContent showContent={showInnerContent}>
+              <h1>Hi!</h1>
+              <h3>{'I\'m Alejo Yarce'}</h3>
+            </WhiteContent>
+            <ImageContainer showContent={showInnerContent}>
+              <ImageContent />
+            </ImageContainer>
+          </InnerContentContainer>
+        </ContentContainer>
+      )}
     </HomeContainer>
   )
 }
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,20 +1,20 @@
 import { mq } from '~app/styles/media'
 import { styled } from '~app/styles/theme'
 
-export const HomeContainer = styled.div`
+export const HomeContainer = styled.div<{ widthRatio: number }>`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: ${({ theme }) => `linear-gradient(0deg, ${theme.colors.primary.white} 50%, ${theme.colors.primary.black} 50%)`};
+  background: ${({ theme, widthRatio }) => `linear-gradient(180deg, ${theme.colors.primary.black} ${widthRatio}%, ${theme.colors.primary.white} 0%)`};
   
-  ${({ theme }) => mq('lg')`
-    background: linear-gradient(90deg, ${theme.colors.primary.black} 50%, ${theme.colors.primary.white} 50%);
+  ${({ theme, widthRatio }) => mq('md')`
+    background: linear-gradient(90deg, ${theme.colors.primary.black} ${widthRatio}%, ${theme.colors.primary.white} 0%);
   `}
 `
-export const ContentContainer = styled.div`
-  height: 60vh;
+export const ContentContainer = styled.div<{ widthRatio: number }>`
+  height: ${({ widthRatio }) => widthRatio}vh;
   width: 80vw;
   max-width: 70rem;
   display: flex;
@@ -22,7 +22,9 @@ export const ContentContainer = styled.div`
   align-items: center;
   background: linear-gradient(0deg, ${({ theme }) => theme.colors.primary.black} 50%, ${({ theme }) => theme.colors.primary.white} 50%);
 
-  ${({ theme }) => mq('lg')`
+  ${({ theme, widthRatio }) => mq('md')`
+    height: 60vh;
+    width: ${widthRatio}vw;
     background: linear-gradient(270deg, ${theme.colors.primary.black} 50%, ${theme.colors.primary.white} 50%);
   `}
 `
@@ -30,8 +32,69 @@ export const InnerContentContainer = styled.div`
   height: calc(100% - 0.5rem);
   width: calc(100% - 0.5rem);
   background: linear-gradient(180deg, ${({ theme }) => theme.colors.primary.black} 50%, ${({ theme }) => theme.colors.primary.white} 50%);
+  color: ${({ theme }) => theme.colors.primary.white};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
 
-  ${({ theme }) => mq('lg')`
+  ${({ theme }) => mq('md')`
     background: linear-gradient(90deg, ${theme.colors.primary.black} 50%, ${theme.colors.primary.white} 50%);
+    flex-direction: row;
+  `}
+`
+export const WhiteContent = styled.div<{ showContent: boolean }>`
+  height: 50%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  opacity: 0;
+  transition: opacity 1s;
+  ${({ showContent }) => showContent && `
+    opacity: 1;
+  `}
+
+  h1 {
+    font-size: 15rem;
+  }
+
+  h3 {
+    font-size: 3em;
+  }
+
+  ${mq('md')`
+    height: 100%;
+    width: 50%;
+  `}
+`
+export const ImageContainer = styled.div<{ showContent: boolean }>`
+  height: 50%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 1s;
+  ${({ showContent }) => showContent && `
+    opacity: 1;
+  `}
+
+  ${mq('md')`
+    height: 100%;
+    width: 50%;
+  `}
+`
+export const ImageContent = styled.div`
+  height: 100%;
+  width: 90%;
+  background-image: url('/AlejoYarce_Doodle.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  ${mq('md')`
+    height: 90%;
   `}
 `
